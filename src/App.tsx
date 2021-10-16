faf4b7b723b27a1eef177479e05cee6a9426a571
Yaroslav Vdovin <52864645+Proza1k@users.noreply.github.com>
2021-10-17 00:20:43 +0300

    Revert "Revert "Revert "Revert "Revert "feat: edit text for app title"""" (#10)" (#11)
    This reverts commit f1649da26af11c7fd09a4c4126e0d1a5560fa5b1.

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to Belover</h1>
+        <h1 className="App-title">Hello world!</h1>
       </header>
       <p className="App-intro">{!data ? "Loading..." : data}</p>
     </div>
